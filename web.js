a2ac9a8abee7743e381a5b5fad586b027db147a0
Ivan Hernando <ivanhdelpino@gmail.com>
2013-07-10 00:07:57 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
-      if (err) throw err;
-      response.send(data.toString());
-})});
+  var data = fs.readFileSync('index.html'); 
+  response.send(data.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
